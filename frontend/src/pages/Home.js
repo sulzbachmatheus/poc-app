80f726288879a25ca1eda71678e2ef96c3d82d1d
leonardocesca <leonardocescaf@gmail.com>
2021-03-12 01:07:03 -0300

    add: front

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import axios from 'axios';
+
+import {
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native';
+
+const Home = () => {
+    
+  const [result, setResult] = React.useState();
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3333/users')
+        .then(res => {
+          setResult(res.data.message)
+        })
+        .catch(err => setResult(err.message))
+  }, []);
+
+  return (
+    <View style={styles.view}>
+      <Text>{result}</Text>  
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
+
+export default Home;
