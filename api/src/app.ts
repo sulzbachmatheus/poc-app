554f3b9b4383978e09def96f6c6f82c5083d7d8e
Matheus_Sulzbach <Matheus_Sulzbach@W10HM3LYY2>
2021-03-12 21:51:12 -0300

    error handlers and middleware for exceptions

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import "express-async-errors";
 import createConnection from "./database";
 import { router } from './routes';
 import cors from 'cors';
+import { AppError } from './errors/AppError';
 
 createConnection();
 const app = express();
@@ -10,6 +12,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
+
+//handling exceptions using middleware concept
+app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
+    if (err instanceof AppError){
+        return res.status(err.statusCode).json({
+            message: err.message
+        });
+    }
+    return res.status(500).json({
+        status: "Error",
+        message: `Internal Server Error ${err.message}`
+    })
+});
 
 //default route
 app.get("/", (req, res) => {
--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepository';
 import * as yup from 'yup';
+import { AppError } from '../errors/AppError';
 
 class UserController {
 
@@ -23,9 +24,7 @@ class UserController {
         try {
             await schema.validate(req.body, { abortEarly: false });
         } catch (err) {
-            return res.status(400).json({
-                error: err
-            });
+            throw new AppError(err);
         }        
         
         const usersRepository = getCustomRepository(UsersRepository);
@@ -35,9 +34,7 @@ class UserController {
         });
 
         if (userAlreadyExists) {
-            return res.status(400).json({
-                error: "User already exists!"
-            });
+            throw new AppError("User already exists");
         }
         
         const user = usersRepository.create({
